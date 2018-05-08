bf4b15a3824a2886e68dfcdbe71d2edd72a49b87
ani-coder-hacker <geetha021970@gmail.com>
2018-05-08 16:25:44 +0530

    Start!

---
+++ accounts/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from accounts.forms import RegistrationForm, EditProfileForm
+from django.contrib.auth.models import User
+
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'accounts/home.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/account/profile')
+    else:
+        form = RegistrationForm()
+
+        args = {'form' : form}
+        return render(request, 'accounts/reg_form.html', args)
+
+
+def view_profile(request):
+    args = {'user' : request.user}
+    return render(request, 'accounts/profile.html', args)
+
+def edit_profile(request):
+    if request.method == 'POST':
+        form = EditProfileForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('/account/profile')
+    else:
+        form = EditProfileForm(instance=request.user)
+        args = {'form': form}
+        return render(request, 'accounts/edit_profile.html', args)
